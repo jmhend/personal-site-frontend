009db524f983add7f97f8d99947380b9a653c6ec
Jesse Hendrickson <jmhendrickson@gmail.com>
2022-09-22 08:59:45 -0400

    added postView share to twitter button

--- components/PostView.tsx
+++ components/PostView.tsx
@@ -1,6 +1,8 @@
 import React, { FunctionComponent } from "react";
 import { Post } from "../models/Post";
 import MarkdownIt from 'markdown-it';
+import Link from "next/link";
+import { useRouter } from 'next/router'
 
 export type PostViewProps = {
     post: Post;
@@ -9,11 +11,40 @@ export type PostViewProps = {
 export const PostView: FunctionComponent<PostViewProps> = ( { post } : PostViewProps) => {
     const md = new MarkdownIt();
     const htmlBody = md.render(post.body);
+    const twitterShareUrl = `https://twitter.com/intent/tweet?url=&{}via=jessetweettweet`
+    
     return (
-        <div className="post-view page-container pt-8 pb-8">
-            <h1 className="page-title mb-2">{post.title}</h1>
-            <h2 className="text-[#726F6C] text-sm mb-8">{post.formatDateAndMinutes()}</h2>
-            <section dangerouslySetInnerHTML={{__html: htmlBody}}></section>
-        </div>
+        <>
+            <div className="post-view page-container pt-8 pb-8">
+                <h1 className="page-title mb-2">{post.title}</h1>
+                <h2 className="text-[#726F6C] text-sm mb-8">{post.formatDateAndMinutes()}</h2>
+                <section dangerouslySetInnerHTML={{__html: htmlBody}}></section>
+            </div>
+            <div className="page-container">
+                <div className="pt-4 pb-4">
+                    <Link href={getTwitterShareUrl(post)}>
+                        <a>
+                        <div className="flex flex-row" >
+                            <img src="/twitter.svg" className="w-4 h-4 mr-1"/>
+                            <p className="text-xs text-slate-600">Share on Twitter</p>
+                        </div>
+                        </a>
+                    </Link>
+
+                </div>
+            </div>
+        </>
     );
+}
+
+function getTwitterShareUrl(post: Post): string  {
+    const { asPath } = useRouter();
+    const qs = require('qs');
+    const url = `www.jmhend.me${asPath}`;
+    const params: string = qs.stringify({
+        text: post.title,
+        via: 'jessetweettweet',
+        url: url
+    });
+    return `https://twitter.com/intent/tweet?${params}`;
 }
